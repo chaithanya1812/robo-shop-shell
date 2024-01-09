074d3e0f7f8b8413b3197373e12e989e4ed5ec7f
CHAITHANYA <111736742+chaithanya1812@users.noreply.github.com>
2024-01-09 20:54:09 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,6 @@
 R="\e[31"
-G"\e[32"
-N="\0[0m"
+G="\e[32"
+N="\e[0m"
 echo -e "[Installing Nginx].."
 dnf install nginx -y > /dev/null
 [[ $? -ne 0 ]] && echo -e "$R [Installing Nginx is Not done!] $N" || echo -e "$G [Installing Nginx done successfully] $N"
