b867defd57b1850d0625407ae4656ac144b2043f
CHAITHANYA <111736742+chaithanya1812@users.noreply.github.com>
2024-01-18 20:14:17 +0530

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -2,10 +2,22 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 ScriptLocation=$(pwd)
+LogFile="/tmp/roboshop.log"
 
 function status_check() {
-if [ $? -ne 0 ]; then
-echo "$R $2 is failed ${N}"
+if [ $1 -ne 0 ]; then
+echo "${R} $2 is failed ${N}"
+echo "${R} Check the log-file in ${LogFile}"
 else
-echo "$R $2 is failed ${N}"
+echo "${R} $2 is failed ${N}"
 }
+
+function print_head() {
+echo "{G} $1 {N}"
+}
+
+print_head "Redis is offering the repo file as a rpm"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> ${LogFile}
+
+status_check $? "Redis is offering the repo file as a rpm"
+
